ba04ea59c5cc65184a5c39dde9cc483408efb74e
雨相羽 <1529403937@qq.com>
2024-07-06 20:50:09 +0800

    7.6

--- js/fakemain/fakemain.js
+++ js/fakemain/fakemain.js
@@ -1,5 +1,3 @@
-const nggyu = new Audio("res/fakemain/nggyu.mp3");
-
 function clearAccount() {
     document.getElementById("account").style.display = "none";
 }
@@ -9,22 +7,43 @@ function init() {
     form.addEventListener("submit", submit);
 }
 
+// 使用localStorage存储用户名和密码
 function submit(event) {
-    event.preventDefault();
-    var username = document.getElementById("user").value;
-    var password = document.getElementById("pwd").value;
+    try {
+        event.preventDefault();
+        var username = document.getElementById("user").value;
+        var password = document.getElementById("pwd").value;
 
-    if (username == "") {
-        alert("请填写用户名(Please fill in the username)");
+        if (username == "") {
+            alert("请填写用户名(Please fill in the username)");
+        }
+        else if (password == "") {
+            alert("请填写密码(Please fill in the password)");
+        }
+        else {
+            if (localStorage.getItem(username) != null) {
+                if (localStorage.getItem(username) == password){
+                    var form = document.getElementById("login_form");
+                    form.action = "fakesuccess.html";
+                    form.method = "get";
+                    form.submit();
+                }
+                else {
+                    alert("密码错误(Incorrect password)");
+                }
+            }
+            else {
+                localStorage.setItem(username, password);
+                localStorage.setItem(username + "_usage", "0.00");
+                var form = document.getElementById("login_form");
+                form.action = "fakesuccess.html";
+                form.method = "get";
+                form.submit();
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        alert("连接错误(Connection error)");
     }
-    else if (password == "") {
-        alert("请填写密码(Please fill in the password)");
-    }
-    else {
-        var form = document.getElementById("login_form");
-        form.action = "fakesuccess.html";
-        form.method = "get";
-        form.submit();
-    }
-    window.session
+    
 }
--- js/fakesuccess/fakesuccess.js
+++ js/fakesuccess/fakesuccess.js
@@ -4,30 +4,32 @@ var time = 0;
 
 
 function init() {
-    time = Math.ceil(Math.random() * maxInit);
+    // time = Math.ceil(Math.random() * maxInit);
 
-    var tmp = time;
-    var sec = tmp % 60;
-    tmp = (tmp - sec) / 60;
-    var min = tmp % 60;
-    tmp = (tmp - min) / 60;
-    var hour = tmp;
+    // var tmp = time;
+    // var sec = tmp % 60;
+    // tmp = (tmp - sec) / 60;
+    // var min = tmp % 60;
+    // tmp = (tmp - min) / 60;
+    // var hour = tmp;
 
-    if (sec < 10) sec = "0" + sec;
-    if (min < 10) min = "0" + min;
-    if (hour < 10) hour = "0" + hour
+    // if (sec < 10) sec = "0" + sec;
+    // if (min < 10) min = "0" + min;
+    // if (hour < 10) hour = "0" + hour;
 
-    var timeStr = hour + ":" + min + ":" + sec;
+    // var timeStr = hour + ":" + min + ":" + sec;
+    var timeStr = "00:00:00";
     document.getElementById("clock").innerHTML = timeStr;
 
     // 解析用户名并修改
     var href = window.location.href;
     var regex = new RegExp(".*\?username=(.*)&password=.*");
     var results = regex.exec(href);
-    document.getElementById("name").innerHTML = decodeURIComponent(results[1]);
+    document.getElementById("name").innerHTML = results[1];
 
     setInterval(updTime, 1000);
-    setUsage();
+    setInterval(updUsage, 1000, results[1]);
+    setUsage(results[1]);
 }
 
 function updTime() {
@@ -48,8 +50,18 @@ function updTime() {
     document.getElementById("clock").innerHTML = timeStr;
 }
 
-function setUsage() {
-    var usage = Math.random() * maxUsage;
-    document.getElementById("usage").style.width = usage + "px";
-    document.getElementById("usage_text").innerHTML = (usage / 268.0 * 125).toFixed(2) + "GB";
+function setUsage(username) {
+    // var usage = Math.random() * maxUsage;
+    var usage = parseFloat(localStorage.getItem(username + "_usage"));
+    document.getElementById("usage").style.width = (usage * 268.0 / 125.0).toFixed(0) + "px";
+    document.getElementById("usage_text").innerHTML = usage.toFixed(2) + "GB";
+}
+
+function updUsage(username) {
+    var usage = parseFloat(localStorage.getItem(username + "_usage"));
+    usage = usage + 3.33;
+    if (usage > 50.00) {
+        usage = 50.00;
+    }
+    localStorage.setItem(username + "_usage", String(usage));
 }
